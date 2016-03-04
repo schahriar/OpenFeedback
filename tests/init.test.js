e9f7761a2cd69edc6bc11d0c08e07371c53aa44f
Schahriar SaffarShargh <info@schahriar.com>
2016-03-04 13:05:12 -0800

    [tests] Added database flush

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,5 +1,12 @@
 // #1
+var exec = require("child_process").exec;
 describe("Initialization Tests", function () {
+  it("should flush database", function (done) {
+    exec("curl \"http://localhost:9200/_all/_flush\"", function (error) {
+      if (error) throw error;
+      done();
+    });
+  });
   it("should create a new instance", function (done) {
     global.app = new OpenFeedback();
     done();
