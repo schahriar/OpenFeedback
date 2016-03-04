737dd388371e688ffacacf16a55b4d17ed3904e3
Schahriar SaffarShargh <info@schahriar.com>
2016-03-04 11:55:14 -0800

    Updated Elastic Driver

--- core/controllers/drivers/elastic.search.js
+++ core/controllers/drivers/elastic.search.js
@@ -7,6 +7,11 @@ const EventEmitter = require("events");
 const elasticsearch = require('elasticsearch');
 // -------- //
 
+/**
+ * @class Elastic
+ * @description SCRUD-based driver, has to emit
+ * `ready` when connected
+ */
 class Elastic extends EventEmitter {
   constructor(options) {
     super();
@@ -17,6 +22,28 @@ class Elastic extends EventEmitter {
       // Emit ready when database is responding
       this.emit("ready", this);
     });
+  }
+  
+  search(query) {
+    return this.client.search(query);
+  }
+  
+  create(document) {
+    return this.client.create(document);
+  }
+  
+  get(index) {
+    return this.client.search({
+      index: index
+    });
+  }
+  
+  update(document) {
+    return this.client.update(document);
+  }
+  
+  delete(query) {
+    return this.client.delete(query);
   }
   
   ping(callback) {
