5a971f6c58aeb229c6e6dff89a2e1df236c56a4b
Schahriar SaffarShargh <info@schahriar.com>
2016-03-04 16:22:19 -0800

    Added corresponding methods to feedback controller

--- core/controllers/feedback.js
+++ core/controllers/feedback.js
@@ -9,24 +9,44 @@ class Feedback extends DefaultController {
     super(schema, driver);
   }
   
-  search() {
-    
+  search(query, callback) {
+    this.driver.search(query, function (error, results) {
+      if (error) return callback(error);
+      
+      callback(null, results);
+    });
   }
   
-  create() {
-    
+  create(document, callback) {
+    this.validate(document, (error) => {
+      if (error) return callback(error);
+      
+      this.driver.create(document, function (error, _document) {
+        if (error) return callback(error);
+        
+        callback(null, _document);
+      });
+    });
   }
   
-  get() {
-    
+  get(query, callback) {
+    this.driver.get(query, callback);
   }
   
-  update() {
-    
+  update(document, callback) {
+    this.validate(document, (error) => {
+      if (error) return callback(error);
+      
+      this.driver.update(document, function (error, _document) {
+        if (error) return callback(error);
+        
+        callback(null, _document);
+      });
+    });
   }
   
-  delete() {
-    
+  delete(query, callback) {
+    this.driver.delete(query, callback);
   }
 }
 
