c4429d8bbe0f6b5cb42fd3ad92bab0f9e0087b1d
Schahriar SaffarShargh <info@schahriar.com>
2016-03-07 19:13:12 -0800

    [tests] Added schema for E2E testing

--- tests/init.test.js
+++ tests/init.test.js
@@ -14,6 +14,19 @@ describe("Initialization Tests", function () {
     global.app = new OpenFeedback();
     done();
   });
+  it("should add E2E test schema", function () {
+    app.addSchema("E2E", {
+      type: 'object',
+      properties: {
+        body: {
+          description: "A feedback",
+          type: 'string',
+          minimum: 10
+        }
+      },
+      required: ['body']
+    });
+  });
   it("should start RESTful API for E2E testing", function () {
     app.connect({ port: 28408 });
   });
